34126b336a2ac735d9410fe2e02065c8f3fd6b6c
R. Scott Webber <swebber@thehwpgroup.com>
2025-11-28 10:22:35 -0500

    Implements password validation logic
    Adds logic to validate passwords based on two different criteria:
    - The number of times a specific character appears within a given range.
    - Whether the specific character appears exactly once at given positions.
    
    Also includes input data to test the logic.

---
+++ Day02/Day02/PasswordEntry.cs
@@ -0,0 +1,22 @@
+﻿namespace Day02;
+
+internal class PasswordEntry
+{
+    public int Min { get; set; }
+    public int Max { get; set; }
+    public char Letter { get; set; }
+    public required string Password { get; set; }
+
+    public bool IsValidByCountPassword()
+    {
+        var count = Password.Count(c => c == Letter);
+        return count >= Min && count <= Max;
+    }
+
+    public bool IsValidByPositionPassword()
+    {
+        bool firstPositionMatches = Password[Min - 1] == Letter;
+        bool secondPositionMatches = Password[Max - 1] == Letter;
+        return firstPositionMatches ^ secondPositionMatches;
+    }
+}
---
+++ Day02/Day02/Program.cs
@@ -0,0 +1,33 @@
+﻿using Day02;
+using System.Text.RegularExpressions;
+
+var input = ParseInput("data01.txt");
+int validCount = input.Count(entry => entry.IsValidByCountPassword());
+int validPositionCount = input.Count(entry => entry.IsValidByPositionPassword());
+Console.WriteLine($"Number of valid passwords (by count): {validCount}");
+Console.WriteLine($"Number of valid passwords (by position): {validPositionCount}");
+
+static List<PasswordEntry> ParseInput(string fn)
+{
+    var appDir = AppDomain.CurrentDomain.BaseDirectory;
+    var filePath = Path.Combine(appDir, "Data", fn);
+
+    var lines = File.ReadAllLines(filePath);
+    var entries = new List<PasswordEntry>();
+
+    foreach (var item in lines)
+    {
+        var match = Regex.Match(item, @"^(\d+)-(\d+) (\w): (\w+)$");
+        if (!match.Success) continue;
+
+        entries.Add(new PasswordEntry
+        {
+            Min = int.Parse(match.Groups[1].Value),
+            Max = int.Parse(match.Groups[2].Value),
+            Letter = match.Groups[3].Value[0],
+            Password = match.Groups[4].Value
+        });
+    }
+
+    return entries;
+}
