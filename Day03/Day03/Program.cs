9b7e2d88556d56b7514b4be7c3fa81aab78e7a73
R. Scott Webber <scott@teagan.org>
2025-11-30 06:24:34 -0500

    day three

---
+++ Day03/Day03/Program.cs
@@ -0,0 +1,23 @@
+﻿using Day03;
+
+var trees = new TreeMap("data01.txt");
+int treeCount = trees.TraverseAndCountTrees(3, 1);
+
+Console.WriteLine($"Trees: {trees.Count}");
+Console.WriteLine($"Height: {trees.Height}");
+Console.WriteLine($"Encountered Trees: {treeCount}");
+
+List<int> treeCounts = new();
+treeCounts.Add(trees.TraverseAndCountTrees(1, 1));
+treeCounts.Add(trees.TraverseAndCountTrees(3, 1));
+treeCounts.Add(trees.TraverseAndCountTrees(5, 1));
+treeCounts.Add(trees.TraverseAndCountTrees(7, 1));
+treeCounts.Add(trees.TraverseAndCountTrees(1, 2));
+
+long product = 1;
+foreach (int count in treeCounts)
+{
+    product *= count;
+}
+
+Console.WriteLine($"Product of encountered trees: {product}");
---
+++ Day03/Day03/TreeMap.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day03;
+
+internal class TreeMap
+{
+    private HashSet<(int row, int col)> _trees = new();
+    private int _width = 0;
+
+    public int Height { get; private set; }
+
+    public TreeMap(string fn)
+    {
+        ParseInput(fn);
+    }
+
+    public bool HasTree(int row, int col)
+    {
+        if (row < 0 || row >= Height)
+            throw new ArgumentOutOfRangeException(nameof(row), "Row is out of bounds.");
+
+        int wrappedCol = col % _width;
+        return _trees.Contains((row, wrappedCol));
+    }
+
+    public int TraverseAndCountTrees(int right, int down)
+    {
+        int treeCount = 0;
+        int row = 0;
+        int col = 0;
+
+        while (row < Height)
+        {
+            if (HasTree(row, col))
+            {
+                treeCount++;
+            }
+
+            row += down;
+            col += right;
+        }
+
+        return treeCount;
+    }
+
+    public int Count { get { return _trees.Count; } }
+
+    private void ParseInput(string fn)
+    {
+        var appDir = AppDomain.CurrentDomain.BaseDirectory;
+        var filePath = Path.Combine(appDir, "Data", fn);
+
+        int row = 0;
+        var lines = File.ReadAllLines(filePath);
+
+        _width = lines[0].Length;
+        Height = lines.Length;
+
+        foreach (var line in lines)
+        {
+            for (int col = 0; col < line.Length; col++)
+            {
+                if (line[col] == '#')
+                {
+                    _trees.Add((row, col));
+                }
+            }
+            row++;
+        }
+    }
+}
